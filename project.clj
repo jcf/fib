3b36df8b4ab8714a272abfabb189efeed3650fb0
James Conroy-Finn <git@invetica.co.uk>
2020-01-09 18:51:41 +0000

    Update project metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject jcf/fib "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Fast Fibonacci implementation using the fast doubling method."
+  :url "https://github.com/jcf/fib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
